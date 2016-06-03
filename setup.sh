6d4afd9462f98fd62a990c64e52770f70a2242ab
Gino Clement <ginoclement@gmail.com>
2016-06-02 21:16:18 -0700

    Cleaning up files

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@
 git clone git@github.com:ginoclement/bash_settings.git
 cp -vf bash_settings/* ~/
 
+
+# Delete unnecessary files
 if [ -f setup.sh ]; do
+	echo "Deleting Setup Script"
 	rm setup.sh
 fi
+
+echo "Deleting README.md"
+rm README.md
+
